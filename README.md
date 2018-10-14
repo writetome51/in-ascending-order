292f0949130dd548cb6333cc8bd08cb679fe6f71
Steve Thompson <writetome51@gmail.com>
2018-10-14 15:46:33 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+2 functions:
+
+inAscendingOrder(numbers); // returns boolean
+
+notInAscendingOrder(numbers); // returns boolean
+
+Argument must be array of numbers, not strings containing numbers.
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+
+var in_ascending_order = require("./lib/inAscendingOrder_notInAscendingOrder");
+
+exports.inAscendingOrder = in_ascending_order.inAscendingOrder;
+exports.notInAscendingOrder = in_ascending_order.notInAscendingOrder;
