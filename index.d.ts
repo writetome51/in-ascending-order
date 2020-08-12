bc37989c0d59d51fcaf3ac3f65e73ebc67ad9533
Steve Thompson <writetome51@gmail.com>
2020-08-12 13:26:07 -0600

    Once again, refactored code to optimize speed

---
+++ index.d.ts
@@ -0,0 +1,1 @@
+export declare function inNumericOrder(numbers: number[]): boolean;
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import {errorIfNotFiniteNumber} from 'error-if-not-finite-number';
 import {errorIfLengthIsZero} from 'error-if-length-is-zero';
-import {hasValue} from '@writetome51/has-value-no-value';
 
 
 export function inNumericOrder(numbers) {
@@ -8,12 +7,11 @@ export function inNumericOrder(numbers) {
 	// Better to put this here than inside the loop:
 	errorIfNotFiniteNumber(numbers[0]);
 
-	let i = 0;
-	while (hasValue(numbers[i + 1])) {
-		errorIfNotFiniteNumber(numbers[i + 1]);
+	let i = 0, length = numbers.length;
+	while (++i + 1 <= length) {
+		errorIfNotFiniteNumber(numbers[i]);
 
-		if (numbers[i] > numbers[i + 1]) return false;
-		++i;
+		if (numbers[i-1] > numbers[i]) return false;
 	}
 	return true;
 }
